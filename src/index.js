5fa434a473893de5c4020c015385ce5dc42e2dd2
andrenoid <79751083+andrenoid@users.noreply.github.com>
2021-03-06 11:15:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 module.exports = function reverse (n) {
-      let str = String(n);
-    str = str.split('').reverse().join('');
-    return +str;
+    let str;
+    if (n >= 0) {
+        str = String(n);
+        str = str.split('').reverse().join('');
+        return +str;
+    } else {
+        n = n * (-1);
+        str = String(n);
+        str = str.split('').reverse().join('');
+        str = Number(str);
+        return str * (-1);
+    }
 }
